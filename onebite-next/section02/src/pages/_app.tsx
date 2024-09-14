84b10633b566dbf4b4ad899f8769bf39825bb754
HyeIn <helen6662@naver.com>
2024-09-14 15:11:43 +0900

    [feat] 페이지별 레이아웃 설정하기

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
+import {ReactNode} from "react";
 
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps }: AppProps & {Component:NextPageWithLayout}) {
+    type NextPageWithLayout = NextPage &{
+        getLayout?:(page: ReactNode) =>ReactNode;
+    }
+    const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
+
   return(
     <GlobalLayout>
-      <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
     </GlobalLayout>
   )
 }
