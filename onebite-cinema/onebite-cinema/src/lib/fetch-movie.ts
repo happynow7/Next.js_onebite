0557a5f3ca18e3042ed281048f621ac60857d608
HyeIn <helen6662@naver.com>
2024-09-17 02:25:26 +0900

    [feat] 서치 페이지 데이터 페칭 구현(SSR)

--- onebite-cinema/onebite-cinema/src/lib/fetch-movie.ts
+++ onebite-cinema/onebite-cinema/src/lib/fetch-movie.ts
@@ -1,8 +1,12 @@
 import { MovieData } from "@/types";
 
-export default async function fetchMovies(): Promise<MovieData[]> {
+export default async function fetchMovies(
+    q?: string
+): Promise<MovieData[]> {
     let url = `http://localhost:12345/movie`;
-
+    if (q) {
+        url += `/search?q=${q}`;
+    }
 
     try {
         const response = await fetch(url);
--- onebite-cinema/onebite-cinema/src/pages/search/index.tsx
+++ onebite-cinema/onebite-cinema/src/pages/search/index.tsx
@@ -1,11 +1,28 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import fetchMovies from "@/lib/fetch-movie";
 import { ReactNode, useEffect, useState } from "react";
 import MovieItem from "@/components/movie-item";
 import style from "./search.module.css";
 import movies from "@/mock/dummy.json";
+import {GetServerSidePropsContext} from "next";
+import {InferGetServerSidePropsType} from "next";
 
-export default function Page() {
+
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
+    const q = context.query.q;
+    const movies = await fetchMovies(q as string);
+
+    return {
+        props: {
+            movies,
+        },
+    };
+};
+
+export default function Page({movies}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const router = useRouter();
     const [filteredMovies, setFilteredMovies] = useState(movies);
 
