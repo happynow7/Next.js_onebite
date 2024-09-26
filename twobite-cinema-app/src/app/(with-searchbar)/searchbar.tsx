5003fdd93760237484760d5672d547538070c2d9
HyeIn <helen6662@naver.com>
2024-09-26 16:04:42 +0900

    [feat] 서치바 파라미터 전달

--- twobite-cinema-app/src/app/(with-searchbar)/searchbar.tsx
+++ twobite-cinema-app/src/app/(with-searchbar)/searchbar.tsx
@@ -1,15 +1,41 @@
 "use client";
-import {useState} from "react";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function Searchbar(){
-    const [search, setSearch] = useState("")
-    const oonChangeSearch = (e:React.ChangeEvent<HTMLInputElement>)=>{
+export default function Searchbar() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const [search, setSearch] = useState("");
+    const q = searchParams.get("q");
+
+    useEffect(() => {
+        setSearch(q || "");
+    }, [q]);
+
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
-    }
+    };
+
+    const onSubmit = () => {
+        if (!search || q === search) return;
+        router.push(`/search?q=${ search }`);
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            onSubmit();
+        }
+    };
+
     return (
         <div>
-            <input/>
-            <button>검색</button>
+            <input
+                onChange={onChangeSearch}
+                onKeyDown={onKeyDown}
+                value={search}
+                placeholder="검색어를 입력해주세요"
+            />
+            <button onClick={onSubmit}>검색</button>
         </div>
-    )
+    );
 }
