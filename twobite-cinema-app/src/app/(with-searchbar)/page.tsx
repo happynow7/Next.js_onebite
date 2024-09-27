7ceb0851193a2566e963e5e672136aaf94208325
HyeIn <helen6662@naver.com>
2024-09-28 02:53:56 +0900

    [feat] 누락 및 수정 코드 업로드, 인덱스 페이지  Static 설정

--- twobite-cinema-app/src/app/(with-searchbar)/page.tsx
+++ twobite-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -1,54 +1,52 @@
 import MovieItem from "../../components/movie-item";
 import style from "./page.module.css";
+import {MovieData} from "@/types";
 
-    async function AllMovies(){
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {cache: "force-cache"})
-        if(!response.ok){
-            return <div>오류가 발생했습니다..</div>
-        }
-        const allMovies: MovieData[] = await response.json();
-        return (
-            <div className={style.all_container}>
-                {allMovies.map((movie) => (
-                    <MovieItem key={`all-${movie.id}`} {...movie} />
-                ))}
-            </div>
-        )
+async function AllMovies(){
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {cache: "force-cache"})
+    if(!response.ok){
+        return <div>오류가 발생했습니다..</div>
+    }
+    const allMovies: MovieData[] = await response.json();
+    return (
+        <div className={style.all_container}>
+            {allMovies.map((movie) => (
+                <MovieItem key={`all-${movie.id}`} {...movie} />
+            ))}
+        </div>
+    )
+}
+
+async function RecoMovies() {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`, { next:{revalidate:10} })
+    if (!response.ok) {
+        return <div>오류가 발생했습니다...</div>;
     }
 
-    async function RecoMovies() {
-         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
-            { next: { revalidate: 10 } }
-        );
-        if (!response.ok) {
-            return <div>오류가 발생했습니다...</div>;
-        }
-
-        const recoMovies: MovieData[] = await response.json();
-        return (
-            <div className={style.reco_container}>
-                {recoMovies.map((movie) => (
-                    <MovieItem key={`reco-${movie.id}`} {...movie} />
-                ))}
-            </div>
-        );
-    }
+    const recoMovies: MovieData[] = await response.json();
+    return (
+        <div className={style.reco_container}>
+            {recoMovies.map((movie) => (
+                <MovieItem key={`reco-${movie.id}`} {...movie} />
+            ))}
+        </div>
+    );
+}
 
 
 
 export default function Home() {
 
     return (
-    <div className={style.conatiner}>
-      <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <RecoMovies/>
-      </section>
-      <section>
-        <h3>등록된 모든 영화</h3>
-        <AllMovies/>
-      </section>
-    </div>
-  );
+        <div className={style.conatiner}>
+            <section>
+                <h3>지금 가장 추천하는 영화</h3>
+                <RecoMovies/>
+            </section>
+            <section>
+                <h3>등록된 모든 영화</h3>
+                <AllMovies/>
+            </section>
+        </div>
+    );
 }
