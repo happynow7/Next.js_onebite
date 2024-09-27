a839f2b3edd0408c2173ec68bd71edfa0adb4dca
HyeIn <helen6662@naver.com>
2024-09-28 02:54:37 +0900

    [feat] footer 추가

--- twobite-cinema-app/src/app/layout.tsx
+++ twobite-cinema-app/src/app/layout.tsx
@@ -1,6 +1,23 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import {MovieData} from "@/types";
+
+async  function Footer(){
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {cache: "force-cache"})
+
+  if(!response.ok){
+    return <footer>제작 @winterlood @happynow7</footer>
+  }
+  const movies: MovieData[] = await response.json();
+  const movieCount = movies.length;
+
+  return<footer>
+    <div className={style.footer}>제작 @winterlood @happynow7</div>
+    <div className={style.footer}>총 {movieCount}개의 영화가 등록되어 있습니다.</div>
+  </footer>
+
+}
 
 export default function RootLayout({
   children,
@@ -15,6 +32,7 @@ export default function RootLayout({
             <Link href={"/"}>TWOBITE CINEMA</Link>
           </header>
           <main>{children}</main>
+          <Footer/>
         </div>
       </body>
     </html>
