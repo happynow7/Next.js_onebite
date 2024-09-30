6e0ea9ec317e5016f402a5510fdb54f0feee9e0d
HyeIn <helen6662@naver.com>
2024-09-30 17:43:43 +0900

    [feat] 로딩스피너 배치 및 딜레이 시간 부여

--- twobite-cinema-app/src/app/(with-searchbar)/search/page.tsx
+++ twobite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -1,8 +1,11 @@
 import MovieItem from "../../../components/movie-item";
 import style from "./page.module.css";
 import {MovieData} from "@/types";
+import {delay} from "@/util/delay";
 
 export default async function Page({searchParams}:{searchParams: {q?:string}}) {
+
+  await delay(1000);
 
   const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${searchParams.q}`,
