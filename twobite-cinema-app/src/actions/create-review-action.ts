5c97cc35d23c79df5c29ce726c7325982f7d056e
HyeIn <helen6662@naver.com>
2024-10-08 16:18:03 +0900

    [feat] 리뷰 추가 기능 업그레이드(로딩UI, 로딩 상태관리, 중복 제출 방지 .etc)

--- twobite-cinema-app/src/actions/create-review-action.ts
+++ twobite-cinema-app/src/actions/create-review-action.ts
@@ -1,23 +1,39 @@
 "use server";
 
-import {revalidatePath} from "next/cache";
+import { delay } from "@/util/delay";
+import { revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
     const movieId = formData.get("movieId")?.toString();
     const content = formData.get("content")?.toString();
     const author = formData.get("author")?.toString();
 
     if (!movieId || !content || !author) {
-        return;
+        return {
+            status: false,
+            error: "리뷰 내용과 작성자를 입력해주세요",
+        };
     }
 
     try {
+        await delay(2000);
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
             method: "POST",
             body: JSON.stringify({ movieId, content, author }),
-        });  revalidatePath(`/movie/${movieId}`);
+        });
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        revalidateTag(`review-${movieId}`);
+        return {
+            status: true,
+            error: "",
+        };
     } catch (err) {
-        console.error(err);
-        return;
+        return {
+            status: false,
+            error: `리뷰 저장에 실패했습니다 : ${err}`,
+        };
     }
 }
