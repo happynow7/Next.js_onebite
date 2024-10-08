4bb2d0f62093a38656455461c2fd88563326eff6
HyeIn <helen6662@naver.com>
2024-10-08 22:26:59 +0900

    [feat] vercel 배포 설정

--- onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,7 @@ import {delay} from "@/util/delay";
 import {Suspense} from "react";
 import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { Metadata } from "next";
 
 async function AllBooks(){
     await delay(1500)
@@ -38,15 +39,15 @@ async function RecoBooks (){
 
 export const dynamic = "force-dynamic";
 
-export const metadata : Metadata={
+export const metadata: Metadata = {
     title: "한입 북스",
-    description : "한입 북스에 등록된 도서를 만나보세요.",
-    openGraph:{
-        title:"한입 북스",
-        description: "한입 북스에 등록된 도서를 만나보세요.",
-        image: ["/thumbnail.png"],
-    }
-}
+    description: "한입 북스에 등록된 도서를 만나보세요",
+    openGraph: {
+        title: "한입 북스",
+        description: "한입 북스에 등록된 도서를 만나보세요",
+        images: ["/thumbnail.png"],
+    },
+};
 
 export default function Home() {
   return (
